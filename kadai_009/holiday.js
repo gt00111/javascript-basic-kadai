d45e675340bc06862fdd44efa8f36a7107821c90
gt00111 <165282338+gt00111@users.noreply.github.com>
2024-05-25 14:32:01 +0900

    appuro-do

---
+++ kadai_009/holiday.js
@@ -0,0 +1,14 @@
+const holiday = ["正月","成人式","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
+// console.log(holiday);
+
+let index = 0;
+// while文
+while (index < holiday.length) {
+   console.log(holiday[index]);
+   index++;
+}
+
+// for文
+for (let i = 0; i < holiday.length; i++){
+    console.log(holiday[i]);
+}
